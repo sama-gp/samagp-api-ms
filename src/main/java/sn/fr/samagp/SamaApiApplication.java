591df427d19d26b6188c2b0e1879c3af33ea8b68
NDIAYE Khazim <k.ndiaye@PC466.domaine.local>
2025-02-28 16:00:09 +0100

    Init project

---
+++ src/main/java/sn/fr/samagp/SamaApiApplication.java
@@ -0,0 +1,13 @@
+package sn.fr.samagp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamaApiApplication.class, args);
+	}
+
+}
